ae4d4a7046b4bfef499e38ca4c67195efbce12a5
benjamin <529709737@qq.com>
2020-07-23 13:48:14 +0800

    [FizzBuzz][Benjamin] test: pass should_return_FizzBuzzWhizz_when_count_off_given_105

--- src/test/java/junit5/tdd/FizzBuzzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzTest.java
@@ -95,4 +95,17 @@ public class FizzBuzzTest {
         //then
         Assertions.assertEquals("BuzzWhizz", result);
     }
+
+    @Test
+    void should_return_FizzBuzzWhizz_when_count_off_given_105(){
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int inputNumber = 105;
+
+        //when
+        String result = fizzBuzz.play(inputNumber);
+
+        //then
+        Assertions.assertEquals("FizzBuzzWhizz", result);
+    }
 }
