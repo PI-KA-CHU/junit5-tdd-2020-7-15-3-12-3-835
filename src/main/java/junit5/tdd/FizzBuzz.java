ef8a6964933492365ea1cd2b9c3551a7fafec40d
benjamin <529709737@qq.com>
2020-07-23 13:11:22 +0800

    [FizzBuzz][Benjamin] feature: pass should_return_Buzz_when_count_off_given_5

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -3,9 +3,17 @@ package junit5.tdd;
 public class FizzBuzz {
 
     public String play(int number){
+        String result = "";
         if (number % 3 == 0) {
-            return "Fizz";
+            result += "Fizz";
         }
-        return String.valueOf(number);
+        if (number % 5 == 0) {
+            result += "Buzz";
+        }
+
+        if (result.isEmpty()) {
+            return String.valueOf(number);
+        }
+        return result;
     }
 }
